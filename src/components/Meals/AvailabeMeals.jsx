d503354f406c73f36fb439f6e69e8aa72b756df8
shubham kumar <devshubhamyadav@gmail.com>
2022-04-27 17:41:00 +0530

    complete

--- src/components/Meals/AvailabeMeals.jsx
+++ src/components/Meals/AvailabeMeals.jsx
@@ -1,49 +1,86 @@
 import React from "react";
-import Card from "../UI/Card";
+import SushiImg from "../../assets/sushi.jpg";
+import SchnitzelImg from "../../assets/Schnitzel.jpg";
+import BarbecueBurgerImg from "../../assets/BarbecueBurger.jpeg";
+import GreenBowlImg from "../../assets/GreenBowl.jpeg";
 import MealItem from "./MealItem/MealItem";
+
 const DUMMYMEALS = [
   {
     id: "m1",
     name: "Sushi",
     description: "Finest fish and veggies",
+    images: SushiImg,
     price: 22.99,
   },
   {
     id: "m2",
     name: "Schnitzel",
     description: "A german specialty!",
+    images: SchnitzelImg,
     price: 16.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
     description: "American, raw, meaty",
+    images: BarbecueBurgerImg,
     price: 12.99,
   },
   {
     id: "m4",
     name: "Green Bowl",
     description: "Healthy...and green...",
+    images: GreenBowlImg,
+    price: 18.99,
+  },
+  {
+    id: "m5",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    images: SushiImg,
+    price: 22.99,
+  },
+  {
+    id: "m6",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    images: SchnitzelImg,
+    price: 16.5,
+  },
+  {
+    id: "m7",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    images: BarbecueBurgerImg,
+    price: 12.99,
+  },
+  {
+    id: "m8",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    images: GreenBowlImg,
     price: 18.99,
   },
 ];
 const AvailabeMeals = () => {
   return (
-    <section className="max-w-5xl w-11/12 mx-auto my-8 ">
-      <Card>
-        <ul>
-          {DUMMYMEALS.map((meal) => (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {DUMMYMEALS?.map((meal) => (
             <MealItem
               id={meal.id}
               key={meal.id}
               name={meal.name}
+              imageSrc={meal.images}
               description={meal.description}
               price={meal.price}
             ></MealItem>
           ))}
-        </ul>
-      </Card>
-    </section>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,19 +1,41 @@
 import React from "react";
-import MealItemForm from "./MealItemForm";
+//import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   return (
-    <li className="flex justify-between m-4 border-b-2 border-gray-500">
-      <div>
-        <h1 className="m-1">{props.name}</h1>
-        <div className="italic">{props.description}</div>
-        <div className="mt-1 font-bold color-indigo-500 text-lg">
-          ${props.price.toFixed(2)}
+    <div key={props.id} className="group relative">
+      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <img
+          src={props.imageSrc}
+          alt={props.imageAlt}
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-lg text-gray-700">
+            <span aria-hidden="true" className="absolute inset-0" />
+            {props.name}
+          </h3>
+          <p className="mt-1 text-base text-gray-500">{props.description}</p>
         </div>
+        <p className="text-lg font-medium text-indigo-600">
+          ${props.price.toFixed(2)}
+        </p>
       </div>
-      <div>
-        <MealItemForm id={props.id} />
-      </div>
-    </li>
+    </div>
+
+    // <li className="flex justify-between m-4 border-b-2 border-gray-500">
+    //   <div>
+    //     <h1 className="m-1">{props.name}</h1>
+    //     <div className="italic">{props.description}</div>
+    //     <div className="mt-1 font-bold color-indigo-500 text-lg">
+    //       ${props.price.toFixed(2)}
+    //     </div>
+    //   </div>
+    //   <div>
+    //     <MealItemForm id={props.id} />
+    //   </div>
+    // </li>
   );
 };
 
